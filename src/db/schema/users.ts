ca85f97ecc87c4a787279afa4bc3344b07662e81
dfilter <dirk_filter@outlook.com>
2025-07-29 19:22:20 -0400

    make user email unique

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -4,7 +4,7 @@ import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 export const users = pgTable("users", {
   id: uuid().primaryKey(),
   name: text().notNull(),
-  email: text().notNull(),
+  email: text().notNull().unique(),
   roles: text()
     .array()
     .notNull()
